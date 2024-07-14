189bec65e966d605a3e2786d3492a740ce7b4596
Nandira Paturohman <nandirasp@gmail.com>
2024-07-14 21:20:57 +0700

    feat: add clerk and router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+// Import the layouts
+import RootLayout from './components/layouts/root-layout'
+import DashboardLayout from './components/layouts/dashboard-layout'
+
+// Import the components
+import IndexPage from './routes'
+import ContactPage from './routes/contact'
+import SignInPage from './routes/sign-in'
+import SignUpPage from './routes/sign-up'
+
+// Import the dashboard components
+import DashboardPage from './routes/dashboard'
+import InvoicesPage from './routes/dashboard/invoices'
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <IndexPage /> },
+      { path: "/contact", element: <ContactPage /> },
+      { path: "/sign-in/*", element: <SignInPage /> },
+      { path: "/sign-up/*", element: <SignUpPage /> },
+      {
+        element: <DashboardLayout />,
+        path: "dashboard",
+        children: [
+          { path: "/dashboard", element: <DashboardPage /> },
+          { path: "/dashboard/invoices", element: <InvoicesPage /> }
+        ]
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
